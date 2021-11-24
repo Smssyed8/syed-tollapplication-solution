0f81a14db5f173e5512d2396bc1727f08bd1c9c3
syed <s.muazzam.siddiqhi@gmail.com>
2021-11-24 20:58:08 +0530

    updated timedService
    updated timedService

--- src/main/java/com/tollapplication/service/GetTimedFeeServiceImpl.java
+++ src/main/java/com/tollapplication/service/GetTimedFeeServiceImpl.java
@@ -1,46 +1,18 @@
 package com.tollapplication.service;
 
-import com.tollapplication.exceptions.TollApplicationException;
 import com.tollapplication.utils.StaticDataUtil;
-import com.tollapplication.utils.constants.ServiceConstants;
+import com.tollapplication.utils.mappers.TimeFeeDto;
 
 import java.time.LocalTime;
 
 public class GetTimedFeeServiceImpl implements GetTimedFeeService {
 
     /**
-     * assuming the map will be properly configured in yml
-     * assuming default value sometimes is skipped in yml
+     * get fee for given time, in parallel, default fee to 8, considering Indian scenario
      * @param time
      * @return
      */
     @Override
-    public double getFeeForTime(LocalTime time) {
-         return time.getMinute() < 30 ?
-                getFee(time, ServiceConstants.FIRST_HALF) : getFee(time, ServiceConstants.SECOND_HALF);
-    }
-
-    /**
-     * Considering time Fee configured as per in timeBasedFeeMapping.yml
-     * Default fee is 8
-     * @param time
-     * @param firstOrSecondHalf
-     * @return
-     */
-    private Double getFee(LocalTime time, String firstOrSecondHalf) {
-        try {
-            return StaticDataUtil.timeFeeMap.containsKey(String.valueOf(time.getHour())) ?
-                    (Double.valueOf(StaticDataUtil.timeFeeMap
-                            .get(String.valueOf(time.getHour()))
-                            .get(firstOrSecondHalf)
-                            .get(ServiceConstants.FEE))) : ServiceConstants.DEFAULT_FEE;
-        } catch(Exception e){
-            throw new TollApplicationException(e.getMessage());
-        }
-    }
-
-    //Another way of getting fee from list of timefee
-    /*@Override
     public double getFeeForTime(LocalTime time) {
        return StaticDataUtil.timeFeeDtosList
       .stream()
@@ -51,6 +23,12 @@ public class GetTimedFeeServiceImpl implements GetTimedFeeService {
       .orElse(8d);
     }
 
+    /**
+     * Find fee within start and end time for given time
+     * @param timeFeeDto
+     * @param time
+     * @return
+     */
     private boolean isWithinTimeList(TimeFeeDto timeFeeDto, LocalTime time) {
             LocalTime startTime = timeFeeDto.getStart();
             LocalTime endTime = timeFeeDto.getEnd();
@@ -58,6 +36,5 @@ public class GetTimedFeeServiceImpl implements GetTimedFeeService {
                 return endTime.equals(time) || endTime.isAfter(time);
             }
             return false;
-    };*/
-
+    }
 }
